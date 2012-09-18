4085a1e7fc6ff035e2af18a1a82c426541f7881d
Msquared <msquared@ubuntu.(none)>
2012-09-18 17:29:18 -0500

    most recently, still boke

--- input.c
+++ input.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define MAXLINE 10
+#define MAXLINE 1
 
 void user_input()
 {
-    char curr;
-    int count = 0;
+    char **arryPtr = (char**)malloc(sizeof(char*));
+int count = 0;
+char curr;
+    char* complete = "l";
+    char* second = "s";
+    char* third = "p";
+    /*SHOULD SWITCH THIS TO reALLOC but worry about it later*/
+    char* temp = (char*)malloc((strlen(complete)+2) * sizeof(char));
+    complete = strcpy(temp, complete);
+    complete = strcat(temp,second);
+
+    temp = (char*)malloc((strlen(complete)+2) * sizeof(char));
+    complete = strcpy(temp, complete);
+    complete = strcat(temp, third);
+
+    printf("%s", complete);
+/*This is working to build a string of variable length. . Now we need to add it to our growing array of Char*/
+    printf("%i", arryPtr);
+    arryPtr = memmove(arryPtr, complete,sizeof(char*));
+    printf("%s", *arryPtr);
 
     while (count < MAXLINE && EOF != curr)
     {
@@ -15,4 +34,5 @@ void user_input()
 	curr = getchar(); /*Capture the enter key*/
         count = count + 1;
     }
-}
+
+ }
